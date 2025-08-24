4862f325ebf23f35b0282c2262f501f4dec3479c
omshewalegit <omshewale20205050@gmail.com>
2025-08-24 10:02:43 +0530

    logging and exception handling

---
+++ src/logger.py
@@ -0,0 +1,28 @@
+import logging
+import os
+from datetime import datetime
+
+# Log file ka naam (date-time ke sath)
+LOG_FILE = f"app_log_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log"
+
+# Logs folder ka path
+logs_path = os.path.join(os.getcwd(), "logs")
+os.makedirs(logs_path, exist_ok=True)
+
+# File ka complete path
+LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
+
+# Logging configuration
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    handlers=[
+        logging.FileHandler(LOG_FILE_PATH),   # File me log save hoga
+        logging.StreamHandler()               # Console par bhi log dikhayega
+    ]
+)
+
+if __name__ == "__main__":
+    logging.info("Logging has started")
+    logging.warning("This is a warning")
+    logging.error("This is an error")
